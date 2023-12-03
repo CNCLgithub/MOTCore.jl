a9e884f5484a168f598f162ba8fa615675347094
belledon <mbelledonne@gmail.com>
2023-12-03 13:34:21 -0500

    simplified dataset to EEE,DDD,EMD

--- test/repulsion.jl
+++ test/repulsion.jl
@@ -40,7 +40,7 @@ function gen_init(wm::RepulsionWM, dur::Int64, low_tdd, high_tdd)
         accept = tdd > low_tdd && tdd < high_tdd
         print("tdd: $(tdd) \r")
     end
-    return part
+    return part, tdd
 end
 
 function gen_part(st::RepulsionState, wm::RepulsionWM, dur::Int64, low_tdd, high_tdd)
@@ -53,7 +53,7 @@ function gen_part(st::RepulsionState, wm::RepulsionWM, dur::Int64, low_tdd, high
         accept = tdd > low_tdd && tdd < high_tdd
         print("tdd: $(tdd) \r")
     end
-    return part
+    return part, tdd
 end
 
 function gen_trial(wm::RepulsionWM, tot_dur::Int64, parts)
@@ -61,17 +61,19 @@ function gen_trial(wm::RepulsionWM, tot_dur::Int64, parts)
     dur_part = Int64(tot_dur / nparts)
     # burnin period to prevent objects from occluding
     trial = RepulsionState[]
-    @show parts
+    tdds = Vector{Float64}(undef, nparts)
     for (i, (low, high)) = enumerate(parts)
         local part
-        @time part = if i == 1
-            gen_init(wm, dur_part, low, high)
+        local tdd
+        @time if i == 1
+            (part, tdd) = gen_init(wm, dur_part, low, high)
         else
-            gen_part(last(trial), wm, dur_part, low, high)
+            (part, tdd) = gen_part(last(trial), wm, dur_part, low, high)
         end
         trial = vcat(trial, part)
+        tdds[i] = tdd
     end
-    return trial
+    return trial, tdds
 end
 
 function tddensity(state::RepulsionState)
@@ -101,7 +103,7 @@ function burnin(wm::RepulsionWM, steps::Int64)
     return state
 end
 
-function warmup(wm::RepulsionWM, k::Int64 = 240, n::Int64 = 1000)
+function warmup(wm::RepulsionWM, k::Int64=240, n::Int64=1000)
     m = tddensity(burnin(wm, 12))
     v = 0.0
     c = 1
@@ -114,36 +116,34 @@ function warmup(wm::RepulsionWM, k::Int64 = 240, n::Int64 = 1000)
         v += (x - m_prev) * (x - m)
         c += 1
     end
-    (m, sqrt(v/(c -1)))
+    (m, sqrt(v / (c - 1)))
 end
 
 function test()
     wm = RepulsionWM(;
-                     n_dots = 8,
-                     wall_repulsion = 10.0,
-                     dot_repulsion = 12.0,
-                     distance_factor = 37.0,
-                     max_distance = 200.0,
-                     vel = 7.0,
-                     max_accel = 4.0,
-                     max_vel = 10.0,
-                     force_sd = 1.0,
-                     )
+        n_dots=8,
+        wall_repulsion=10.0,
+        dot_repulsion=12.0,
+        distance_factor=37.0,
+        max_distance=200.0,
+        vel=7.0,
+        max_accel=4.0,
+        max_vel=10.0,
+        force_sd=1.0,
+    )
 
     # dataset parameters
     tot_dur = 360  # 15s
 
     @time tdd = tdd_mu, tdd_sd = warmup(wm, tot_dur, 1000)
-    low_part  = [0.0,                    tdd_mu - 4.00 * tdd_sd]
-    mid_part  = [tdd_mu - 1.00 * tdd_sd, tdd_mu + 1.00 * tdd_sd]
-    high_part = [tdd_mu + 6.00 * tdd_sd, Inf                  ]
-
-    parts = [low_part, mid_part ,high_part]
+    D = [0.0, tdd_mu - 4.00 * tdd_sd]
+    M = [tdd_mu - 1.00 * tdd_sd, tdd_mu + 1.00 * tdd_sd]
+    E = [tdd_mu + 6.00 * tdd_sd, Inf]
 
     @show tdd
-    @show low_part
-    @show mid_part
-    @show high_part
+    @show D
+    @show M
+    @show E
 
     dataset = []
     cond_list = []
@@ -152,23 +152,33 @@ function test()
 
     # trial = gen_trial(wm, tot_dur, [[0.0, Inf]])
     # render_scene(wm, trial, "$(base)/test")
-    
-    perms = [[low_part, low_part, low_part],
-             [mid_part, mid_part, mid_part],
-             [high_part, high_part, high_part],
-             [low_part, mid_part, high_part],
-             [low_part, high_part, mid_part],
-             [mid_part, low_part, high_part]]
 
+    perms = [[D, D, D],
+             [E, E, E],
+             [D, M, E],
+             [D, M, E],
+             [E, M, D],
+             [E, M, D]]
+
+    df = DataFrame(:scene => Int32[],
+                   :epoch => Int32[],
+                   :tdd => Float32[])
+    # total of 12 trials
     for (i, perm) = enumerate(perms)
-        trial = gen_trial(wm, tot_dur, perm)
-        # render_scene(wm, trial, "$(base)/$(i)")
+        ne = length(perm)
+        trial, epochs = gen_trial(wm, tot_dur, perm)
+        @show perm
+        @show epochs
         push!(dataset, trial)
         push!(cond_list, [i, false])
         push!(cond_list, [i, true])
+        append!(df, Dict(:scene => i,
+                         :epoch => 1:ne,
+                         :tdd => epochs))
     end
     write_dataset(dataset, "test/output/dataset.json")
     write_condlist(cond_list, "test/output/trial_list.json")
+    CSV.write("test/output/tdd.csv", df)
 end
 
 test();
