b1c1007f6cbb7ed7d47907af829fa4a2a6fda7f8
belledon <mbelledonne@gmail.com>
2024-07-05 16:27:56 -0400

    added colonoscopy exp

--- test/render_dataset.jl
+++ test/render_dataset.jl
@@ -16,7 +16,7 @@ targets = [true, true, true, true, false, false, false, false]
 
 
 function main()
-    dname = "0.5.1"
+    dname = "colonoscopy_0.1"
     local dataset
     open("test/output/$(dname)/dataset.json", "r") do f
         dataset = JSON.parse(f)
